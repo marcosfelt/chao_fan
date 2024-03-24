5ca72f2e89aec0c94513623944fc1cc348ee1e16
Kobi Felton <kobi.c.f@gmail.com>
2024-03-23 23:07:21 -0400

    🔄 Refactor recipe enrichment process
    - Refactor function names and logic for recipe enrichment process
    - Update to use recipe_scrapers for scraping recipe information
    - Implement ingredient extraction

---
+++ chao_fan/integrations/recipe_scrapers.py
@@ -0,0 +1,157 @@
+from datetime import datetime
+from typing import Dict, List, Optional
+import nltk
+from recipe_scrapers import scrape_me, WebsiteNotImplementedError
+from recipe_scrapers._exceptions import SchemaOrgException, NoSchemaFoundInWildMode
+from ingredient_parser import parse_ingredient
+from chao_fan.models import Recipe, Instruction, RecipeIngredient, IngredientNutrition
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def create_instructions(
+    instructions: str,
+    instructions_list: Optional[List[str]] = None,
+) -> List[Instruction]:
+    """Create instructions from a string"""
+    if instructions_list is None:
+        instructions_list = instructions.split("\n")
+    return [
+        Instruction(step_number=i, step=step)
+        for i, step in enumerate(instructions_list)
+    ]
+
+
+def download_nltk_model():
+    nltk.download("averaged_perceptron_tagger")
+
+
+def estimate_ingredient_price(ingredient: RecipeIngredient) -> float:
+    return 0.5
+
+
+def estimate_ingredient_nutrition(
+    ingredient: RecipeIngredient,
+) -> IngredientNutrition | None:
+    return {}
+
+
+def create_ingredients(ingredients_txt: List[str]) -> List[RecipeIngredient]:
+    """Create ingredients from a list of strings"""
+    parsed_ingredients = []
+    for ingredient_txt in ingredients_txt:
+        if ingredient_txt is None:
+            continue
+        # Create ingredient
+        ingredient = RecipeIngredient(
+            full_description=ingredient_txt,
+        )
+
+        # Try to parse using ingredient parser
+        parsed_ingredient = parse_ingredient(ingredient_txt)
+        if parsed_ingredient is None:
+            ingredient.name = ingredient_txt
+            parsed_ingredients.append(ingredient)
+            logger.debug("Could not parse: {}")
+            continue
+
+        # Parse name
+        ingredient.name = (
+            parsed_ingredient.name.text if parsed_ingredient.name is not None else None
+        )
+        if ingredient.name is None:
+            ingredient.name = ingredient_txt
+        if len(parsed_ingredient.amount) > 0:
+            ingredient.amount = parsed_ingredient.amount[0].quantity
+            ingredient.unit = parsed_ingredient.amount[0].unit
+
+        # Estimate ingredient price
+        ingredient.estimated_price_100grams = estimate_ingredient_price(ingredient)
+
+        # Estimate ingredient nutrition
+        # ingredient_nutrition = estimate_ingredient_nutrition(ingredient)
+        # if ingredient_nutrition:
+        #     ingredient.ingredient_nutrition = ingredient_nutrition
+
+        parsed_ingredients.append(ingredient)
+    return parsed_ingredients
+
+
+def scrape_recipe(url: str) -> Recipe | None:
+    """
+    Scrape a recipe from a URL
+
+    Parameters
+    ----------
+    url : str
+        The URL of the recipe
+
+
+    Returns
+    -------
+    Recipe
+        The scraped recipe
+
+    """
+    scraper = None
+    wild_mode = False
+    tries = 0
+    recipe = Recipe(source_url=url)
+    while not scraper and tries < 3:
+        try:
+            scraper = scrape_me(url, wild_mode=wild_mode)
+            tries += 1
+        except WebsiteNotImplementedError:
+            wild_mode = True
+        except NoSchemaFoundInWildMode as e:
+            recipe.enrichment_failed_at = datetime.now()
+            logger.error(e)
+            return recipe
+    if scraper is None:
+        recipe.enrichment_failed_at = datetime.now()
+        return recipe
+
+    try:
+        recipe.title = scraper.title()
+        instructions_text = scraper.instructions()
+        instructions_list = scraper.instructions_list()
+    except SchemaOrgException as e:
+        logger.error(e)
+        recipe.enrichment_failed_at = datetime.now()
+        return recipe
+    except TypeError as e:
+        recipe.enrichment_failed_at = datetime.now()
+        logger.error(e)
+        return recipe
+
+    # Instructions
+    if instructions_text:
+        instructions = create_instructions(instructions_text, instructions_list)
+        if len(instructions) > 0:
+            recipe.instructions = instructions
+
+    # Ingredients
+    ingredients = create_ingredients(scraper.ingredients())
+    if len(ingredients) > 0:
+        recipe.recipe_ingredients = ingredients
+
+    # Total time
+    try:
+        total_time = scraper.total_time()
+        if total_time:
+            recipe.ready_in_minutes = total_time
+    except SchemaOrgException as e:
+        logger.error(e)
+
+    # Image
+    try:
+        image = scraper.image()
+    except SchemaOrgException as e:
+        logger.error(e)
+        image = None
+    if image:
+        recipe.image = image
+
+    recipe.enriched_at = datetime.now()
+    return recipe
--- chao_fan/pipelines/update_recipe_db.py
+++ chao_fan/pipelines/update_recipe_db.py
@@ -1,11 +1,12 @@
-from typing import Dict, List
+from datetime import datetime, timedelta
+from typing import Dict, List, Optional
 from chao_fan.integrations.pinterest import (
     setup_pinterest,
     get_pinterest_board_id,
     get_pin_links,
     Pin,
 )
-from chao_fan.integrations.spoonacular import extract_recipe
+from chao_fan.integrations.recipe_scrapers import scrape_recipe
 from dotenv import load_dotenv
 import os
 from chao_fan.models import Recipe
@@ -89,35 +90,77 @@ def insert_pins_into_db(pins: List[Pin], engine: Engine):
             recipe = Recipe(
                 source_name=pin.site_name,
                 source_url=pin.url,
-                spoonacular_enriched=False,
+                enriched=False,
             )
             session.add(recipe)
         session.commit()
 
 
-def _enrich_recipes_spoonacular_batch(session: Session, recipes: List[Recipe], n: int):
+def _enrich_recipes_batch(session: Session, recipes: List[Recipe], n: int):
+    """
+    1. Use recipe_scrapers to scrape recipe (title, instructions and ingredients)
+    2. Extract ingredients using ingredient_parser
+    3. Estimate each ingredient nutrition via semantic search in nutrition table
+    4. Estimate each ingredient price via semantic search in prices table
+    5. Generate recipe embedding
+    6. Estimate recipe preference score using KNN on recipe embeddings
+    """
     for recipe in tqdm(recipes, desc="Enriching", total=n):
-        enriched_recipe = extract_recipe(recipe.source_url)
-        recipe_data = recipe.model_dump(exclude_unset=True)
-        enriched_recipe.sqlmodel_update(recipe_data)
+        enriched_recipe = scrape_recipe(recipe.source_url)
+        if enriched_recipe is None:
+            continue
+        # recipe_data = recipe.model_dump(exclude_unset=True)
+        # enriched_recipe.sqlmodel_update(recipe_data)
         session.add(enriched_recipe)
+        session.delete(recipe)
+
+
+# def _enrich_recipes_spoonacular_batch(session: Session, recipes: List[Recipe], n: int):
+#     for recipe in tqdm(recipes, desc="Enriching", total=n):
+#         enriched_recipe = extract_recipe(recipe.source_url)
+#         recipe_data = recipe.model_dump(exclude_unset=True)
+#         enriched_recipe.sqlmodel_update(recipe_data)
+#         session.add(enriched_recipe)
 
 
 def enrich_recipes_spoonacular(
-    engine: Engine, max_api_calls: int = 150, batch_size: int = 10
+    engine: Engine,
+    max_api_calls: int = 150,
+    batch_size: int = 10,
+    retry_enrichment_after: Optional[timedelta] = None,
 ):
+    """
+    Retrieve recipes from the database and enrich them
+
+    Parameters
+    ----------
+    engine : Engine
+        The sqlalchemy engine
+    max_api_calls : int
+        The maximum number of API calls to make
+    batch_size : int
+        The number of recipes to enrich at a time
+    retry_enrichment_after : timedelta, optional
+        The time after which to retry enrichment, if None is passed, defaults to 1 day
+    """
     # Query for recipes that need to be enriched
+    if retry_enrichment_after is None:
+        retry_enrichment_after = timedelta(days=1)
     i = 0
     batch_size = batch_size if batch_size < max_api_calls else max_api_calls
     while i < max_api_calls:
         with Session(engine) as session:
+            now = datetime.now()
             statement = (
                 select(Recipe)
-                .where(Recipe.spoonacular_enriched == False)
+                .where(
+                    (Recipe.enriched_at == None)
+                    | (Recipe.enrichment_failed_at < now - retry_enrichment_after)
+                )
                 .limit(batch_size)
             )
             recipes = session.exec(statement)
-            _enrich_recipes_spoonacular_batch(session, recipes, batch_size)
+            _enrich_recipes_batch(session, recipes, batch_size)
             session.commit()
             i += batch_size
 
@@ -129,13 +172,13 @@ def update_recipe_db():
 
     # Get pinterest links
     logger.info("Getting pinterest links")
-    pins = get_pinterest_links(BOARD_NAME)
-    new_pins = find_pins_not_in_db(pins, engine)
-    logger.info(f"Found {len(new_pins)} new pins. Inserting into recipe table.")
-    insert_pins_into_db(new_pins, engine)
+    # pins = get_pinterest_links(BOARD_NAME)
+    # new_pins = find_pins_not_in_db(pins, engine)
+    # logger.info(f"Found {len(new_pins)} new pins. Inserting into recipe table.")
+    # insert_pins_into_db(new_pins, engine)
 
     # Enrich recipes
-    logger.info("Enriching recipes with Spoonacular")
+    logger.info("Enriching recipes")
     try:
         enrich_recipes_spoonacular(engine, max_api_calls=MAX_SPOONACULAR_API_CALLS)
     except ValueError as e:
