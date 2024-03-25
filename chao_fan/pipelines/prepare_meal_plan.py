bdbac8f63482848e51b01fc3b6e0072260215be5
Kobi Felton <kobi.c.f@gmail.com>
2024-03-24 20:57:28 -0400

    Remove Llama dependency
    commit-id:35cad6f8

--- chao_fan/pipelines/prepare_meal_plan.py
+++ chao_fan/pipelines/prepare_meal_plan.py
@@ -1,12 +1,13 @@
-from typing import List
-from chao_fan.integrations.llama import (
-    get_model,
-    generate_cuisine_ingredients,
-    generate_themes,
-    MealPlanTheme,
-)
-import logging
 import json
+
+# from chao_fan.integrations.llama import (
+#     get_model,
+#     generate_cuisine_ingredients,
+#     generate_themes,
+#     MealPlanTheme,
+# )
+import logging
+from typing import List
 
 logger = logging.getLogger(__name__)
 
@@ -22,27 +23,27 @@ CUISINES_NAMES = [
 ]
 
 
-def generate_meal_plan_themes() -> List[MealPlanTheme]:
-    """Generate meal themes for different cuisines using llama"""
-    # Get Phi-2 model
-    logger.info("Downloading Phi-2 model")
-    model = get_model()
+# def generate_meal_plan_themes() -> List[MealPlanTheme]:
+#     """Generate meal themes for different cuisines using llama"""
+#     # Get Phi-2 model
+#     logger.info("Downloading Phi-2 model")
+# model = get_model()
 
-    logger.info("Generating cuisine ingredients")
-    cuisines = generate_cuisine_ingredients(model, CUISINES_NAMES)
+# logger.info("Generating cuisine ingredients")
+# cuisines = generate_cuisine_ingredients(model, CUISINES_NAMES)
 
-    logger.info("Generating themes")
-    themes = generate_themes(model=model, cuisines=cuisines)
-    return themes
-
-
-def prepare_meal_plan():
-    generate_meal_plan_themes()
+# logger.info("Generating themes")
+# themes = generate_themes(model=model, cuisines=cuisines)
+# return themes
 
 
-if __name__ == "__main__":
-    logging.basicConfig(
-        level=logging.INFO,
-        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    )
-    prepare_meal_plan()
+# def prepare_meal_plan():
+#     generate_meal_plan_themes()
+
+
+# if __name__ == "__main__":
+#     logging.basicConfig(
+#         level=logging.INFO,
+#         format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+#     )
+#     prepare_meal_plan()
